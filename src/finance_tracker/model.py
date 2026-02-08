ee6f0f57a54671636ebf43aa4527594706c14d6b
Edu Lukmon <edulukmon@gmail.com>
2026-02-08 12:52:22 +0100

    added signed_amount,is_income,is_expense method using @property

--- src/finance_tracker/model.py
+++ src/finance_tracker/model.py
@@ -11,4 +11,53 @@ class Transaction:
     date: str = ""
     type: str = "expense"
     category: Optional[str] = None
-    
+
+    def __post_init__(self):
+        """Run validation on all fields after dataclass initialization."""
+        self._validate()
+
+    def _validate(self):
+        """Central method for all business rules, Validate all init fields"""
+        if self.amount < 0:
+            raise ValueError("Amount cannot be negative")
+        if self.type not in ["expense", "income"]:
+            raise ValueError("Type must be either expense or income")
+        if not self.date:
+            raise ValueError("Date cannot be empty, format must be in YYYY-MM-DD")
+
+        if not re.match(r"^\d{4}-\d{2}-\d{2}$", self.date):
+            raise ValueError("Date must be in YYYY-MM-DD format")
+
+        try:
+            datetime.strptime(self.date, "%Y-%m-%d")
+        except ValueError:
+            raise ValueError("Invalid date!!, Must be a real calender date")
+
+        @property
+        def signed_amount(self) -> float:
+            """Returns positive for income and negative for expense"""
+            return self.amount if self.type == "income" else -self.amount
+
+        @property
+        def is_income(self) -> bool:
+            """Returns if the transaction is an income transaction"""
+            return self.type == "income"
+
+        @property
+        def is_expense(self) -> bool:
+            """Returns if the transaction is an expense transaction"""
+            return self.type == "expense"
+
+        def __str__(self) -> str:
+            sign = "+" if self.is_income else "-"
+            category = f"[{self.category}]" if self.category else ''
+            return(
+                f"{self.date} {sign}{self.amount:,.2f}"
+                f"{self.description} {category}"
+                f"(id: {self.id if self.id is not None else 'New'})"
+            )
+
+        def __repr__(self) -> str:
+            return f"Transaction(id={self.id}, amount={self.amount}, date='{self.date}', "\
+                    f"type='{self.type}', description='{self.description}' category={self.category})"
+            
