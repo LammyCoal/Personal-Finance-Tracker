5bf3612828e28660b8ab043530dcb8ae44960a3f
Edu Lukmon <edulukmon@gmail.com>
2026-02-24 15:01:01 +0100

    added the add method to the cli.py

--- src/finance_tracker/cli.py
+++ src/finance_tracker/cli.py
@@ -11,3 +11,51 @@ app = typer.Typer(
     help="Personal Finance Tracker CLI",
     add_completion=True,
 )
+
+@app.command()
+def add(
+        amount: float = typer.Argument(..., help="Amount to be added: Must be positive."),
+        date: str = typer.Option(
+            None,
+            "--date",
+            "-d",
+            help="Date of when the transaction is added. YYYY-MM-DD(default: today",
+        ),
+        description: str = typer.Option(
+            '',
+            "--description",
+            "-desc",
+            help="Description of the transaction.",
+        ),
+        type_: str = typer.Option(
+            "expense",
+            "--type",
+            "-t",
+            help="Type of the transaction: income or expense",
+        ),
+        category: Optional[str] = typer.Option(
+            None,
+            "--category",
+            "-c",
+            help="Category( e.g salary, food, gym)",
+        ),
+):
+    """Adds a new transaction"""
+    if date is None:
+        date = datetime.now().strftime("%Y-%m-%d")
+
+    try:
+        tn= Transaction.create_new(
+            amount=amount,
+            date=date,
+            description=description,
+            category=category,
+            type_=type_,
+        )
+        storage = TransactionStorage()
+        new_id = storage.add_transaction(tn)
+        typer.secho(f"✓ Successfully added: {new_id}", fg=typer.colors.GREEN, bold=True)
+        typer.echo(tn)
+    except ValueError as ve:
+        typer.secho(f"Error: {ve}", fg=typer.colors.RED, bold=True, err=True)
+        raise typer.Exit(code=1)
